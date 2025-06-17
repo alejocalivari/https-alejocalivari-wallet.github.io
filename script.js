45c2f737d3df80352e3144b5ea31d900ec90c3fb
alejocalivari <calivarialejo14@gmail.com>
2025-06-17 02:30:44 -0300

    fix: comentado mensaje toast de error visual

--- script.js
+++ script.js
@@ -580,7 +580,7 @@ async function updateAllPrices() {
 
         showToast("✅ Precios actualizados correctamente");
     } catch (error) {
-        console.error("Error updating prices:", error);
+       // console.error("Error updating prices:", error);
         //showToast("❌ Error al actualizar precios");
     }
 }
